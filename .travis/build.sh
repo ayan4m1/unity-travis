e26ca6cc7fe2aff5e50ba2b7fed3e1cce0fe638d
ayan4m1 <andrew@bulletlogic.com>
2022-04-16 18:42:08 -0400

    add skeleton build script

---
+++ .travis/build.sh
@@ -0,0 +1,9 @@
+$UNITY_INSTALL_DIR/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile \
+  -projectPath "$(pwd)/${UNITY_PROJECT_NAME}" \
+  -buildWindows64Player "$(pwd)/build/win64/${UNITY_PROJECT_NAME}.exe" \
+  -quit \
+  | tee "$(pwd)/build/win64.log"
