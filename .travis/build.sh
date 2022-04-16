cf782b59bae29260097de5090e7eb51d2e92bf35
ayan4m1 <andrew@bulletlogic.com>
2022-04-16 19:34:23 -0400

    add missing slashes

--- .travis/build.sh
+++ .travis/build.sh
@@ -6,7 +6,7 @@ $UNITY_INSTALL_DIR/Editor/Unity \
   -silent-crashes \
   -logFile \
   -projectPath "$(pwd)/${UNITY_PROJECT_NAME}" \
-  -username "${UNITY_USER_EMAIL}"
-  -password "${UNITY_USER_PASSWORD}"
+  -username "${UNITY_USER_EMAIL}" \
+  -password "${UNITY_USER_PASSWORD}" \
   -buildWindows64Player "$(pwd)/build/win64/${UNITY_PROJECT_NAME}.exe" \
   -quit
