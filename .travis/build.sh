55b6fb6073a47e23fbe8ebd1fb15ad4a501fa41a
ayan4m1 <andrew@bulletlogic.com>
2022-04-18 11:59:46 -0400

    copy and use license file during builds

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,20 @@
 #! /bin/sh
 
+LICENSE_FILE="./Unity_v2021.x.ulf"
+
+if [ ! -f "${LICENSE_FILE}" ]; then
+  echo "WARNING: No license file found!"
+else
+  echo "Copying license file..."
+  cp "${LICENSE_FILE}" "${UNITY_INSTALL_DIR}/"
+fi
+
 echo "Starting Unity build..."
 
 xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' \
 $UNITY_INSTALL_DIR/Editor/Unity \
   -batchmode \
+  -manualLicenseFile "${UNITY_INSTALL_DIR}/Unity_v2021.x.ulf" \
   -logFile /dev/stdout \
   -projectPath "$(pwd)/${UNITY_PROJECT_NAME}" \
   -username "${UNITY_USER_EMAIL}" \
